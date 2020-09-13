b9539d6125c8595f3f9a991a9821dbde75534fda
Denzel <denzeltlee@gmail.com>
2020-09-13 12:13:04 +0800

    Created skills section

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import SEO from '../components/seo';
 
 import Particles from 'react-particles-js';
 import { BsArrowRight } from 'react-icons/bs';
+import { SiHtml5, SiCss3, SiJavascript, SiPhp, SiReact, SiSass, SiJquery, SiTailwindcss, SiGit, SiGithub, SiNpm, SiGraphql } from 'react-icons/si';
 
 import '../styles/index.scss';
 
@@ -16,7 +17,7 @@ const IndexPage = () => {
         query {
             headDp: file(relativePath: { eq: "head-display-picture.png" }) {
                 childImageSharp {
-                    fluid(maxWidth: 300) {
+                    fluid(maxWidth: 350) {
                         ...GatsbyImageSharpFluid
                     }
                 }
@@ -82,6 +83,77 @@ const IndexPage = () => {
                     </div>
                     <div className="about__image">
                         <Img fluid={data.headDp.childImageSharp.fluid} alt="Head Display Picture" />
+                    </div>
+                </div>
+            </section>
+            <section className="skills">
+                <h3 className="title title__center skills__title">Skills</h3>
+                <div className="skills__container">
+                    <p className="skills__container__title">These are the main languages, frameworks / libraries, and tools that I use / have used.</p>
+                    <div className="skills__container__content">
+                        <div className="skills__skillset">
+                            <h4 className="skills__skillset__title">Languages</h4>
+                            <ul className="skills__skillset__list">
+                                <li className="skills__skillset__item">
+                                    <SiHtml5 className="skills__skillset__icon" />
+                                    <p className="skills__skillset__name">HTML</p>
+                                </li>
+                                <li className="skills__skillset__item">
+                                    <SiCss3 className="skills__skillset__icon" />
+                                    <p className="skills__skillset__name">CSS</p>
+                                </li>
+                                <li className="skills__skillset__item">
+                                    <SiJavascript className="skills__skillset__icon" />
+                                    <p className="skills__skillset__name">JavaScript</p>
+                                </li>
+                                <li className="skills__skillset__item">
+                                    <SiPhp className="skills__skillset__icon" />
+                                    <p className="skills__skillset__name">PHP</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="skills__skillset">
+                            <h4 className="skills__skillset__title">Frameworks / Libraries</h4>
+                            <ul className="skills__skillset__list">
+                                <li className="skills__skillset__item">
+                                    <SiReact className="skills__skillset__icon" />
+                                    <p className="skills__skillset__name">React</p>
+                                </li>
+                                <li className="skills__skillset__item">
+                                    <SiSass className="skills__skillset__icon" />
+                                    <p className="skills__skillset__name">Sass</p>
+                                </li>
+                                <li className="skills__skillset__item">
+                                    <SiJquery className="skills__skillset__icon" />
+                                    <p className="skills__skillset__name">jQuery</p>
+                                </li>
+                                <li className="skills__skillset__item">
+                                    <SiTailwindcss className="skills__skillset__icon" />
+                                    <p className="skills__skillset__name">Tailwind CSS</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="skills__skillset">
+                            <h4 className="skills__skillset__title">Tools</h4>
+                            <ul className="skills__skillset__list">
+                                <li className="skills__skillset__item">
+                                    <SiGit className="skills__skillset__icon" />
+                                    <p className="skills__skillset__name">Git</p>
+                                </li>
+                                <li className="skills__skillset__item">
+                                    <SiGithub className="skills__skillset__icon" />
+                                    <p className="skills__skillset__name">GitHub</p>
+                                </li>
+                                <li className="skills__skillset__item">
+                                    <SiNpm className="skills__skillset__icon" />
+                                    <p className="skills__skillset__name">npm</p>
+                                </li>
+                                <li className="skills__skillset__item">
+                                    <SiGraphql className="skills__skillset__icon" />
+                                    <p className="skills__skillset__name">GraphQL</p>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </section>
