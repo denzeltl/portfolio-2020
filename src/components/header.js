365126002069bc838be24e42c59fc38b940726d3
Denzel <denzeltlee@gmail.com>
2020-09-17 16:09:42 +0800

    Added functions for links on sp nav menu

--- src/components/header.js
+++ src/components/header.js
@@ -34,32 +34,84 @@ const Header = () => {
         <header className={`header ${scrolledNavbar ? 'is-scrolled' : ''}`}>
             <div className="header__inner">
                 <h1 className={`header__logo ${toggledButton ? 'is-toggled' : ''}`}>
-                    <Link to="/">denzeltl</Link>
+                    <Link
+                        onClick={() => {
+                            setToggledButton(false);
+                        }}
+                        to="/"
+                    >
+                        denzeltl
+                    </Link>
                 </h1>
                 <button onClick={toggleNavbar} className="header__burger">
                     <IoIosMenu />
                 </button>
                 <nav className={`header__nav ${toggledButton ? 'is-toggled' : ''}`}>
-                    <Link activeClassName="is-active" to="/">
+                    <Link
+                        onClick={() => {
+                            setToggledButton(false);
+                        }}
+                        activeClassName="is-active"
+                        to="/"
+                    >
                         Home
                     </Link>
-                    <Link activeClassName="is-active" to="/about">
+                    <Link
+                        onClick={() => {
+                            setToggledButton(false);
+                        }}
+                        activeClassName="is-active"
+                        to="/about"
+                    >
                         About
                     </Link>
-                    <Link activeClassName="is-active" to="/projects">
+                    <Link
+                        onClick={() => {
+                            setToggledButton(false);
+                        }}
+                        activeClassName="is-active"
+                        to="/projects"
+                    >
                         Projects
                     </Link>
-                    <Link className="is-contact" activeClassName="is-active" to="/contact">
+                    <Link
+                        onClick={() => {
+                            setToggledButton(false);
+                        }}
+                        className="is-contact"
+                        activeClassName="is-active"
+                        to="/contact"
+                    >
                         Contact
                     </Link>
                     <ul className="header__links">
-                        <a href="#" className="header__link">
+                        <a
+                            onClick={() => {
+                                setToggledButton(false);
+                            }}
+                            href="#"
+                            className="header__link"
+                        >
                             <IoIosMail className="header__icon" />
                         </a>
-                        <a href="#" target="_blank" className="header__link">
+                        <a
+                            onClick={() => {
+                                setToggledButton(false);
+                            }}
+                            href="#"
+                            target="_blank"
+                            className="header__link"
+                        >
                             <IoLogoLinkedin className="header__icon" />
                         </a>
-                        <a href="#" target="_blank" className="header__link">
+                        <a
+                            onClick={() => {
+                                setToggledButton(false);
+                            }}
+                            href="#"
+                            target="_blank"
+                            className="header__link"
+                        >
                             <IoLogoGithub className="header__icon" />
                         </a>
                     </ul>
